6cb0ab0d7d55ca5d16854f52ed71e82d448892cc
Manuel Fondovila <fondovila@gmail.com>
2023-03-28 21:59:36 -0300

    add clase 3

---
+++ Clase3/Pages/Index.cshtml.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Clase3.Models;
+using Clase3.Services;
+
+namespace Clase3.Pages;
+
+public class IndexModel : PageModel
+{
+    public List<Movie> MovieList{ get; set;}
+    
+    [BindProperty]
+    public Movie NewMovie { get; set; }
+
+    public IndexModel()
+    {
+        // Constructor
+    }
+
+    public void OnGet()
+    {
+        MovieList = MovieService.GetAll();
+    }
+
+    public IActionResult OnPost(){
+        if(!ModelState.IsValid){
+            return RedirectToPage("/Error");
+        }
+        
+        MovieService.Add(NewMovie);
+
+        return RedirectToAction("get");
+    }
+}
